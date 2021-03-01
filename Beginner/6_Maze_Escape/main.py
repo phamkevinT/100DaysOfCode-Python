ab4554d50bc780af837b0bcd6318df68da784dfc
Kevin Pham <phambamkevin@gmail.com>
2021-03-01 13:34:34 -0800

    escaping a maze

---
+++ Beginner/6_Maze_Escape/main.py
@@ -0,0 +1,22 @@
+# Maze Escape
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+# Prevent robot from walking in a constant loop
+while front_is_clear():
+    move()
+turn_left()
+    
+# Robot hugs the right side of maze
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
