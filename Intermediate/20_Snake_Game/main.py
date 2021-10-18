adcb071cc30578109196ef4d5e66e6e6701c52cd
Kevin Pham <phambamkevin@gmail.com>
2021-10-18 16:54:32 -0700

    Control snake using key arrows

--- Intermediate/20_Snake_Game/main.py
+++ Intermediate/20_Snake_Game/main.py
@@ -8,8 +8,17 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
+# Create a snake object
 snake = Snake()
 
+# Listen for arrow key and bind to snake functions
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+# Update the screen every 0.1 seconds
 game_is_on = True
 while game_is_on:
     screen.update()
--- Intermediate/20_Snake_Game/snake.py
+++ Intermediate/20_Snake_Game/snake.py
@@ -2,6 +2,10 @@ from turtle import Turtle
 
 STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
@@ -9,8 +13,10 @@ class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
+        """Create a snake with 3 initial segments"""
         for position in STARTING_POSITION:
             new_segment = Turtle("square")
             new_segment.color("white")
@@ -19,8 +25,29 @@ class Snake:
             self.segments.append(new_segment)
 
     def move(self):
+        """Move the snake horizontally to the right by default"""
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        """Move the snake direction upwards"""
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        """Move the snake direction downwards"""
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        """Move the snake direction to the left"""
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        """Move the snake direction to the right"""
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
