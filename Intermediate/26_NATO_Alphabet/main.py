b790c24bc6188888e5b31214226270e1a386d56b
Kevin Pham <phamtkevin@gmail.com>
2022-02-28 21:42:09 -0800

    NATO Alphabet

---
+++ Intermediate/26_NATO_Alphabet/main.py
@@ -0,0 +1,17 @@
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+# Create a dictionary
+phonetic_dictionary = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dictionary)
+
+# Create a list of phonetic code words from user's input
+word = input("Enter a word: ").upper()
+output_list = [phonetic_dictionary[letter] for letter in word]
+print(output_list)
+
+
